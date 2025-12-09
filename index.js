58d70b7ddf68d2d4e120635a231c3d5318f10a99
Esa <esaruhulla01@gmail.com>
2025-12-09 22:59:50 +0600

    add: user apis

--- index.js
+++ index.js
@@ -19,8 +19,61 @@ const client = new MongoClient(uri, {
     }
 });
 
+async function run() {
+    try {
+        await client.connect();
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const db = client.db(process.env.DB_NAME);
+        const usersCollection = db.collection('users');
+
+        // ✅user releted Apis here
+
+        // CREATE user
+        app.post('/users', async (req, res) => {
+            try {
+                const newUser = req.body;
+                const result = await usersCollection.insertOne(newUser);
+                res.status(201).json(result);
+            } catch (err) {
+                res.status(500).json({ message: 'Failed to create user', error: err });
+            }
+        });
+
+        // READ ALL users
+        app.get('/users', async (req, res) => {
+            try {
+                const users = await usersCollection.find().toArray();
+                res.json(users);
+            } catch (err) {
+                res.status(500).json({ message: 'Failed to fetch users', error: err });
+            }
+        });
+
+        // READ ONE users
+        app.get('users/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const user = await usersCollection.findOne({ _id: new ObjectId(id) });
+                if (!user) return res.status(404).json({ message: 'User not found' });
+                res.json(user);
+            } catch (err) {
+                res.status(500).json({ message: 'Error finding user', error: err });
+            }
+        });
 
 
+
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 // ✅ Default route
 app.get('/', (req, res) => {
